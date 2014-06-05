86333a9167e21ef90e759369e7574fd9e32f4cfa
landonsanders <mr.landonsanders@yahoo.com>
2014-06-05 10:48:44 -0400

    added compute nearest nieghbor

---
+++ computeNearestNieghbor.py
@@ -0,0 +1,11 @@
+def computeNearestNeighbor(username, users):
+"""creates a sorted list of users based on their distance to
+username"""
+distances = []
+for user in users:
+if user != username:
+distance = manhattan(users[user], users[username])
+distances.append((distance, user))
+# sort based on distance -- closest first
+distances.sort()
+return distances
